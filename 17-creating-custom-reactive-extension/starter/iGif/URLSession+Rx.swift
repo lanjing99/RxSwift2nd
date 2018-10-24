6484a2fdb15363378ac62109ff0c2f9388999e5a
wenjing.chen <wenjing.chen@yingzt.com>
2018-10-24 10:18:28 +0800

    updated ch17

--- 17-creating-custom-reactive-extension/starter/iGif/URLSession+Rx.swift
+++ 17-creating-custom-reactive-extension/starter/iGif/URLSession+Rx.swift
@@ -24,11 +24,73 @@ import Foundation
 import RxSwift
 import SwiftyJSON
 
+extension Reactive where Base: URLSession{
+    func response(request: URLRequest) -> Observable<(HTTPURLResponse, Data)>{
+        return Observable.create{ observer in
+            
+            let task = self.base.dataTask(with: request, completionHandler: { (data, response, error) in
+                guard let response = response, let data = data else{
+                    observer.onError(error ?? RxURLSessionError.unknown)
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else{
+                    observer.onError(RxURLSessionError.invalidResponse(response: response))
+                    return
+                }
+                observer.onNext((httpResponse, data))
+                observer.onCompleted()
+            })
+            task.resume()
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+    
+    func data(request: URLRequest) -> Observable<Data>{
+        return response(request: request).map({ (response, data) -> Data in
+            if 200 ..< 300 ~= response.statusCode{
+                return data
+            }else{
+                throw RxURLSessionError.requestFailed(response: response, data: data)
+            }
+        })
+    }
+    
+    func string(request: URLRequest) -> Observable<String> {
+        return data(request: request).map{ data -> String in
+            return String(data: data, encoding: .utf8) ?? ""
+        }
+    }
+    
+    func image(request: URLRequest) -> Observable<UIImage> {
+        return data(request: request).map{
+            return UIImage.init(data: $0) ?? UIImage()
+        }
+    }
+    
+
+}
+
+extension ObservableType where E == (HTTPURLResponse, Data){
+    func cache() -> Observable<E>{
+        return self.do(onNext: { (response, data) in
+            if let url = response.url?.absoluteString, 200..<300 ~= response.statusCode {
+                internalCache[url] = data
+            }
+        })
+    }
+}
+
+fileprivate var internalCache = [String: Data]()
+
 public enum RxURLSessionError: Error {
   case unknown
   case invalidResponse(response: URLResponse)
   case requestFailed(response: HTTPURLResponse, data: Data?)
   case deserializationFailed
 }
+
 
 
