b8bdafdb086ffd34a9dbdb720f9def5e18cb2b29
wenjing.chen <wenjing.chen@yingzt.com>
2018-10-24 11:13:08 +0800

    ch18 add pods

---
+++ 18-22RxSwfitCommunity/RxCocoaCommunity/RxCocoaCommunity/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  *@项目名称:  RxCocoaCommunity
+//  *@文件名称:  ViewController.swift
+//  *@Date 2018/10/24
+//  *@Author lanjing 
+//  *@Copyright © :  2014-2018 X-Financial Inc.   All rights reserved.
+//  *注意：本内容仅限于小赢科技有限责任公内部传阅，禁止外泄以及用于其他的商业目的。
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
