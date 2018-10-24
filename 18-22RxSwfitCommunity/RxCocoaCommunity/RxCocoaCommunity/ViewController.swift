a392521dbaaaff267707d6345998305b3fd08c9d
wenjing.chen <wenjing.chen@yingzt.com>
2018-10-24 15:08:13 +0800

    ch18 TableView Demo

--- 18-22RxSwfitCommunity/RxCocoaCommunity/RxCocoaCommunity/ViewController.swift
+++ 18-22RxSwfitCommunity/RxCocoaCommunity/RxCocoaCommunity/ViewController.swift
@@ -8,12 +8,32 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let disposeBag = DisposeBag.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let citeis = Observable.of(["Lisbon", "Copenhagen", "London", "Madrid",
+                                    "Vienna"])
+//        citeis.bind(to: tableView.rx.items){ (tableView: UITableView, index: Int, element: String) in
+//            let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+//            cell.textLabel?.text = element
+//            return cell
+//
+//        }.disposed(by: disposeBag)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        citeis.bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+            cell.textLabel?.text = "\(element) @ row \(row)"
+            }
+            .disposed(by: disposeBag)
     }
 
 
